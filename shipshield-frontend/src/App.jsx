e4a2cf08188bc841c3b026e1a1f4f294dc0d6181
Idris jose <idrisjose11@gmail.com>
2026-01-21 13:30:42 +0100

    set up react and tailwind

---
+++ shipshield-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-medium">shipshield-frontend</h1>
+      <p className="text-lg">Welcome to the ShipShield Frontend</p>
+    </>
+  )
+}
+
+export default App
